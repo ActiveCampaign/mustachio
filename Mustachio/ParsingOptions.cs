7ba78d8951c46f613563d575e12ad42c3048c19b
Andrew Theken <andrew@wildbit.com>
2016-06-03 07:45:03 -0700

    Add missing 'ParsingOptions.cs' from previous commit. Permanently fix CRLF issues.

---
+++ Mustachio/ParsingOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Mustachio
+{
+    internal class ParsingOptions
+    {
+        /// <summary>
+        /// If this is true, all values will be rendered without being HTML-encoded. (regardless of using {{{ }}} or {{ }} syntax)
+        /// </summary>
+        internal bool DisableContentSafety { get; set; }
+    }
+}

58a367b2cb27476175e921fa94db17140f3f1962
Andrew Theken <andrew@wildbit.com>
2019-05-16 07:55:26 -0400

    Use descriptive version for nuget package version.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,12 +6,12 @@ dotnet restore
 
 #generate a version, based on the tag, or rev.
 revision=$(git rev-parse HEAD)
-inferred_assembly_version="0.0.0.${TRAVIS_BUILD_NUMBER:-1}"
+inferred_assembly_version="2.0.0.${TRAVIS_BUILD_NUMBER:-0}"
 version=$(echo "${TRAVIS_TAG:-$inferred_assembly_version}" | sed -E s/-.+$//)
-descriptive_version=${TRAVIS_TAG:-"$version-git-$revision"}
+descriptive_version=${TRAVIS_TAG:-"$version-git-${revision:0:6}"}
 
 # build and test in one step.
-dotnet pack -c Release -o ../tmp ./Mustachio/Mustachio.csproj /p:AssemblyVersion=$descriptive_version /p:Version=$descriptive_version
+dotnet pack -c Release -o ../tmp ./Mustachio/Mustachio.csproj -p:AssemblyVersion=$version -p:PackageVersion=$descriptive_version
 
 package_path="../tmp/Postmark.$descriptive_version.nupkg"
 
